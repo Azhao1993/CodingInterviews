eb0faa6a9714cf66e4e75a2030de20c396fde673
andrew <1124913956@qq.com>
2019-01-21 12:43:38 +0800

    建立仓库

---
+++ src/CodingInterviews_04_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.java
@@ -0,0 +1,59 @@
+package CodingInterviews_04_FindInPartiallySortedMatrix;
+
+/*
+	题目描述:
+	在一个二维数组中（每个一维数组的长度相同），
+	每一行都按照从左到右递增的顺序排序，
+	每一列都按照从上到下递增的顺序排序。
+	请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+public class FindInPartiallySortedMatrix {
+	public boolean Find(int target, int[][] array) {
+		if (array == null || array.length == 0 || array[0].length == 0) {
+			return false;
+		}
+		int col = array.length;
+		int row = array[0].length;
+		if (target < array[0][0] || array[col - 1][row - 1] < target) {
+			return false;
+		}
+		int c = 0;
+		while (target > array[c][row - 1]) {
+			c++;
+		}
+		int r = 0;
+		while (target > array[col - 1][r]) {
+			r++;
+		}
+		for (int i = c; i < col; i++) {
+			for (int j = r; j < row; j++) {
+				if (target == array[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// cpp代码改写
+	public boolean Find(int[][] matrix, int rows, int columns, int number) {
+		boolean found = false;
+		if (matrix != null && rows > 0 && columns > 0) {
+			int row = 0;// 行
+			int column = columns - 1;// 列
+			// 每行的最后一个数与target大小关系
+			while (row < rows && column >= 0) {
+
+				if (matrix[row][column] == number) {
+					found = true;
+					break;
+				} else if (matrix[row][column] > number) {
+					--column;
+				} else {
+					++row;
+				}
+			}
+		}
+		return found;
+	}
+}
---
+++ src/CodingInterviews_55_TreeDepth/TheDepthofBinaryTree.java
@@ -0,0 +1,26 @@
+package CodingInterviews_55_TreeDepth;
+
+import TreeNode.TreeNode;
+
+/*
+	题目描述:
+		输入一棵二叉树，求该树的深度。
+		从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+public class TheDepthofBinaryTree {
+	public int TreeDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return getTreeDepth(root, 1, 1);
+	}
+	//递归
+	public int getTreeDepth(TreeNode root, int depth, int result) {
+		if (root == null) {
+			return Math.max(depth - 1, result);
+		}
+		result = Math.max(getTreeDepth(root.left, depth + 1, result), result);
+		result = Math.max(getTreeDepth(root.right, depth + 1, result), result);
+		return result;
+	}
+}
