f6f14624a4d51c792b87e8e9ee354b0ffe7a64bc
andrew <1124913956@qq.com>
2019-01-22 13:17:03 +0800

    10：斐波那契数列

--- src/CodingInterviews_10_Fibonacci/Fibonacci.java
+++ src/CodingInterviews_10_Fibonacci/Fibonacci.java
@@ -8,18 +8,19 @@ package CodingInterviews_10_Fibonacci;
  */
 public class Fibonacci {
 	// 注意：第0项是0
-	public int Fibonacci(int n) {
+	public int fibonacci(int n) {
 		if (n == 0) {
 			return 0;
 		} else if (n == 1) {
 			return 1;
 		} else {
-			return Fibonacci(n - 1) + Fibonacci(n - 2);
+			return fibonacci(n - 1) + fibonacci(n - 2);
 		}
 	}
 
 	// cpp版本改写
 	// ====================方法1：递归====================
+	// 效率很低的解法，挑剔的面试官不会喜欢
 	public long Fibonacci_Solution1(int n) {
 		if (n <= 0)
 			return 0;
@@ -31,6 +32,7 @@ public class Fibonacci {
 	}
 
 	// ====================方法2：循环====================
+	// 面试官期待的实用解法
 	public long Fibonacci_Solution2(int n) {
 		int[] result = { 0, 1 };
 		if (n < 2)
@@ -39,16 +41,18 @@ public class Fibonacci {
 		long fibNMinusOne = 1;
 		long fibNMinusTwo = 0;
 		long fibN = 0;
+		// f(n)=f(n-1)+f(n-2)
+		// f(n-2)=f(n-1)
+		// f(n-1)=f(n)
 		for (int i = 2; i <= n; ++i) {
 			fibN = fibNMinusOne + fibNMinusTwo;
-
 			fibNMinusTwo = fibNMinusOne;
 			fibNMinusOne = fibN;
 		}
-
 		return fibN;
 	}
 
 	// ====================方法3：基于矩阵乘法====================
+	// 时间复杂度O（log n）但不实用的解法
 
 }
