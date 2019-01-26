e4837605973dc6e2043d8609c9592387e22c9a0d
Andrew <Andrew@118.202.43.24>
2019-01-26 23:15:28 +0800

    57（二）：为s的连续正数序列

--- src/CodingInterviews_57_NumbersWithSum/ContinuousSquenceWithSum.java
+++ src/CodingInterviews_57_NumbersWithSum/ContinuousSquenceWithSum.java
@@ -1,11 +1,12 @@
-package CodingInterviews_57_ContinuousSquenceWithSum;
+package CodingInterviews_57_NumbersWithSum;
 
 import java.util.ArrayList;
 
 /*
 	面试题57（二）：为s的连续正数序列
 		输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
-		例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、 4～6和7～8。
+		例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，
+		所以结果打印出3个连续序列1～5、 4～6和7～8。
  */
 public class ContinuousSquenceWithSum {
 	public static void main(String[] args) {
