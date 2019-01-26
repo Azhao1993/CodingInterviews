f011cc4c58f43bec8be069fa2ab146548b57eb6c
Andrew <Andrew@118.202.43.24>
2019-01-26 23:15:11 +0800

    57（一）：和为s的两个数字

--- src/CodingInterviews_57_NumbersWithSum/TwoNumbersWithSum.java
+++ src/CodingInterviews_57_NumbersWithSum/TwoNumbersWithSum.java
@@ -1,11 +1,11 @@
-package CodingInterviews_57_ContinuousSquenceWithSum;
+package CodingInterviews_57_NumbersWithSum;
 
 import java.util.ArrayList;
 
 /*
 	面试题57（一）：和为s的两个数字
-		输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们
-		的和正好是s。如果有多对数字的和等于s，输出任意一对即可。
+		输入一个递增排序的数组和一个数字s，在数组中查找两个数，
+		使得它们的和正好是s。如果有多对数字的和等于s，输出任意一对即可。
  */
 public class TwoNumbersWithSum {
 	public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
