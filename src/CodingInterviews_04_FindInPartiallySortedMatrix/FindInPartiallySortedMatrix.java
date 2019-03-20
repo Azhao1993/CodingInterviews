511c346a6be2573562e96cc508a980b2c380d70f
andrew <1124913956@qq.com>
2019-03-20 21:30:21 +0800

    4：二维数组中的查找

--- src/CodingInterviews_04_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.java
+++ src/CodingInterviews_04_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.java
@@ -8,7 +8,7 @@ package CodingInterviews_04_FindInPartiallySortedMatrix;
 	请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 */
 public class FindInPartiallySortedMatrix {
-	//面试题4：
+	// 面试题4：
 	public boolean Find(int target, int[][] array) {
 		if (array == null || array.length == 0 || array[0].length == 0) {
 			return false;
@@ -36,21 +36,22 @@ public class FindInPartiallySortedMatrix {
 		return false;
 	}
 
-	// cpp代码改写
+	// cpp代码改写/左神算法课
 	public boolean Find(int[][] matrix, int rows, int columns, int number) {
 		boolean found = false;
 		if (matrix != null && rows > 0 && columns > 0) {
 			int row = 0;// 行
 			int column = columns - 1;// 列
-			// 每行的最后一个数与target大小关系
+			// A（row,column）为起点
 			while (row < rows && column >= 0) {
-
 				if (matrix[row][column] == number) {
 					found = true;
 					break;
 				} else if (matrix[row][column] > number) {
+					// 大,向前移动
 					--column;
 				} else {
+					// 小，想下移动
 					++row;
 				}
 			}
