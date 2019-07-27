d48b40b9897c2001c5e5468c5bf59735b4fde410
andrew <1124913956@qq.com>
2019-07-27 15:38:53 +0800

    34：二叉树中和为某一值的路径

--- src/CodingInterviews_34_PathInTree/PathInTree.java
+++ src/CodingInterviews_34_PathInTree/PathInTree.java
@@ -10,48 +10,29 @@ import TreeNode.TreeNode;
 		从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
  */
 public class PathInTree {
-	public static void main(String[] args) {
-		TreeNode root = new TreeNode(10);
-		TreeNode node5 = new TreeNode(5);
-		TreeNode node12 = new TreeNode(12);
-		TreeNode node4 = new TreeNode(4);
-		TreeNode node7 = new TreeNode(7);
-		root.left = node5;
-		root.right = node12;
-		node5.left = node4;
-		node5.right = node7;
-		int target = 22;
-		new PathInTree().FindPath(root, target);
-
-	}
-
 	public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		FindPath(root, target, 0, new ArrayList<Integer>(), result);
-		return result;
+		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+		if (root == null) {
+			return res;
+		}
+		getPath(root, target, new ArrayList<Integer>(), res);
+		return res;
 	}
 
-	private void FindPath(TreeNode root, int target, int sum, ArrayList<Integer> arrayList,
-			ArrayList<ArrayList<Integer>> result) {
-
-		if (root == null && sum != target)
+	private void getPath(TreeNode root, int target, ArrayList<Integer> cur, ArrayList<ArrayList<Integer>> res) {
+		if (root == null || target < 0) {
 			return;
-
-		if (sum < target) {
-			arrayList.add(root.val);
-			sum += root.val;
-			if (root.left == null && root.right == null) {
-				if (sum == target) {
-					result.add(new ArrayList(arrayList));					
-				}
-				arrayList.remove(arrayList.size()-1);
-				sum -= root.val;
-				return;
-			}
-			FindPath(root.left, target, sum, arrayList, result);
-			FindPath(root.right, target, sum, arrayList, result);
-			arrayList.remove(arrayList.size()-1);
+		}
+		if (root.left == null && root.right == null && root.val == target) {
+			cur.add(root.val);
+			res.add(new ArrayList<>(cur));
+			cur.remove(cur.size() - 1);
+			return;
 		}
 
+		cur.add(root.val);
+		getPath(root.left, target - root.val, cur, res);
+		getPath(root.right, target - root.val, cur, res);
+		cur.remove(cur.size() - 1);
 	}
 }
